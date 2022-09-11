91000c8309b921a44d6b480030af833efcc3d6cf
Артур Файзрахманов <heraldhoi@gmail.com>
2022-09-11 14:39:19 +0300

    Use all-the-icons in modeline

--- init.el
+++ init.el
@@ -552,8 +552,8 @@ This function is called immediately after `dotspacemacs/init', before layer
 configuration.
 It is mostly for variables that should be set before packages are loaded.
 If you are unsure, try setting them in `dotspacemacs/user-config' first."
+  (setq dotspacemacs-mode-line-theme '(all-the-icons :separator arrow))
 )
-
 
 (defun dotspacemacs/user-load ()
   "Library to load while dumping.
